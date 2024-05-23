fc2f5c21e247d85607ec9849d6c6c18906281a3b
vakser <65089446+vakser@users.noreply.github.com>
2024-05-23 09:46:29 +0300

    Server side shopping basket functionality implemented.

---
+++ src/main/java/com/ecommerce/sportscenter/entity/Basket.java
@@ -0,0 +1,22 @@
+package com.ecommerce.sportscenter.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@RedisHash("Basket")
+public class Basket {
+    @Id
+    private String id;
+    private List<BasketItem> items = new ArrayList<>();
+
+    public Basket(String id){
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/ecommerce/sportscenter/repository/BasketRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.sportscenter.repository;
+
+import com.ecommerce.sportscenter.entity.Basket;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BasketRepository extends CrudRepository<Basket, String> {
+}
